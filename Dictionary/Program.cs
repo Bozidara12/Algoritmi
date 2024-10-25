44a763a3a7a9d52e35bd90861f61d4c4b44c0e9d
Bozidara12 <bozidaralazarova@gmail.com>
2024-10-25 11:35:50 +0300

    firstcommit

---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ukazatelTel=new Dictionary<string, string>();
+
+            ukazatelTel.Add("Ivan Ivanov", "08890790898");
+            ukazatelTel.Add("Angel Stoqnov", "099089765332");
+            ukazatelTel.Add("Miro Asenov", "08876253425");
+            foreach (var d in ukazatelTel)
+            {
+                Console.WriteLine($"{d.Key} {d.Value}");
+            }
+        }
+    }
+}
